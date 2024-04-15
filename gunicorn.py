4c78a89f409f7976b334a7211702178d58ca1ba8
Muhammad Yekini <44311524+myekini@users.noreply.github.com>
2024-04-15 18:45:58 +0100

    optimization

--- gunicorn.py
+++ gunicorn.py
@@ -1,19 +1,18 @@
 import os
 
-# Get the number of CPU cores
-cpu_cores = os.cpu_count()
 
-# Determine the number of workers based on CPU cores
-workers = cpu_cores * 2 + 1
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
 
-# Set the number of threads per worker
-threads = min(4, cpu_cores)  # Limit threads to a reasonable number, e.g., 4 or the number of CPU cores
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
 
-# Bind to 0.0.0.0:8080 for external access
+# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
-# Allow all IPs in X-Forwarded-For header
+
+
 forwarded_allow_ips = '*'
 
-# Specify headers for secure scheme
-secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+
+
