10825392d9f0e8a0662ff0fcfb484ad9e071cdc9
Muhammad Yekini <44311524+myekini@users.noreply.github.com>
2024-05-30 22:12:17 +0100

    optimized mode with transfer learning

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
-import io # type: ignore
-from flask import Flask, request, jsonify
-from tensorflow import keras
-from keras.models import load_model
-from keras.preprocessing.image import img_to_array, load_img
+import io
 import numpy as np
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+cors = CORS(app, resources={r"/*":{"origins":"*"}})
 
 # Replace with actual paths
-MODEL_PATH = '/home/ubuntu/CNN_MODEL/checkpoints/model_keras_leaf.h5'
-CHECKPOINT_PATH = '/home/ubuntu/CNN_MODEL/checkpoints/model_weights-leaf.weights.h5'
-TARGET_NAMES = ['Class 0 (African_Almond)', 'Class 1 (Avocado)', 'Class 2 (Cashew)', 'Class 3 (Guava)','Class 4 (Mango)']
+MODEL_PATH = '/home/ubuntu/CNN/checkpoints/image_classifier.keras'
+TARGET_NAMES = ['African_Almond', 'Avocado', 'Cashew', 'Guava', 'Mango']
 
 # Load the model architecture and weights
 model = load_model(MODEL_PATH)
-model.load_weights(CHECKPOINT_PATH)
-
 
 def preprocess_image(image):
-    # Convert to RGB (if needed)
+    # Convert to RGB
     image = image.convert('RGB')
 
     # Resize the image
-    image = image.resize((224, 224))
+    image = image.resize((256, 256))  # Adjust to match the model input size
 
     # Convert to a NumPy array
     image = img_to_array(image)
@@ -33,16 +32,7 @@ def preprocess_image(image):
     # Add batch dimension
     image = np.expand_dims(image, axis=0)
 
-    # Convert to grayscale
-    image = np.dot(image[...,:3], [0.2989, 0.5870, 0.1140])  # Convert RGB to grayscale
-
-    # Reshape the image for the model's input format
-    image = np.expand_dims(image, axis=-1)
-
     return image
-
-
-
 
 @app.route('/')
 def index():
@@ -80,4 +70,3 @@ def allowed_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
